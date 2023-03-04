27102f25d3420513d07529877d5f5d2f270cfd2a
Lakshmanaswami <126847056+Lakshmanaswami@users.noreply.github.com>
2023-03-04 10:46:09 +0530

    Add files via upload

---
+++ smallest  number.py
@@ -0,0 +1,10 @@
+# python program to find the smallest number
+
+# list of numbers
+a = [5,7,9,14,10,20,4]
+
+# find smallest number using min() function
+smallest = min(a)
+
+# print the smallest number
+print(f'Smallest number in the list is : {smallest}.')
